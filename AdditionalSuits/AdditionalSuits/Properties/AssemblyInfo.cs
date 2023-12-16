023150d5296533f5d4ced4648240917d4a596d0e
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2023-12-17 07:22:44 +1100

    Optimize and add the suit

--- AdditionalSuits/AdditionalSuits/Properties/AssemblyInfo.cs
+++ AdditionalSuits/AdditionalSuits/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdditionalSuits")]
+[assembly: AssemblyTitle("IrisChansAdditionalSuits")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdditionalSuits")]
+[assembly: AssemblyProduct("IrisChansAdditionalSuits")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0c25b026-8574-4dd2-9af2-4265f618ef8a")]
+[assembly: Guid("0F89115D-0180-4A14-BD8E-36299D5B55D9")]
+
 
 // Version information for an assembly consists of the following four values:
 //
